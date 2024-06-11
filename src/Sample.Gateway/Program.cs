43446116f754f00ab2e87d6beb139e1756a867e8
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2024-06-11 09:46:03 +0200

    01_end_MAD-Summit_Reverse-Proxy

--- src/Sample.Gateway/Program.cs
+++ src/Sample.Gateway/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddControllers().AddHateoas();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddGateway()
+                .LoadConfiguration(builder.Configuration.GetSection("Gateway"))
+                .AddRouting();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+app.UseGatewayRouting();
 
 app.Run();
