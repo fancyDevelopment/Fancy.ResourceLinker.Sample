bad69f2522066cd890867cdad4c75daa6550ae49
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2024-06-11 11:49:22 +0200

    03_end_MAD-Summit_Caching

--- src/Sample.Gateway/Controllers/Routes.cs
+++ src/Sample.Gateway/Controllers/Routes.cs
@@ -5,10 +5,10 @@ namespace Sample.Gateway.Controllers;
 public static class Routes
 {
     public static Task<T> GetFlightManagementFlightSummary<T>(this GatewayRouter router) where T : class => 
-        router.GetAsync<T>("FlightManagement", $"/api/flight-management/flights/summary");
+        router.GetCachedAsync<T>("FlightManagement", $"/api/flight-management/flights/summary", TimeSpan.FromSeconds(30));
 
     public static Task<T> GetFlightShoppingFlightSummary<T>(this GatewayRouter router) where T : class => 
-        router.GetAsync<T>("FlightShopping", $"/api/flight-shopping/flights/summary");
+        router.GetCachedAsync<T>("FlightShopping", $"/api/flight-shopping/flights/summary", TimeSpan.FromSeconds(30));
 
     public static Task<List<T>> GetFlightManagementFlights<T>(this GatewayRouter router, string? from, string? to) where T : class => 
         router.GetAsync<List<T>>("FlightManagement", $"/api/flight-management/flights?from={from ?? ""}&to={to ?? ""}");
