b22ac493721996733b30727dd81bc28ce9eec387
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2024-06-11 11:36:34 +0200

    Implemented FlightSearchVm endpoint

---
+++ src/Sample.Gateway/Controllers/Flights/FlightSearchVm.cs
@@ -0,0 +1,12 @@
+﻿using Fancy.ResourceLinker.Models;
+
+namespace Sample.Gateway.Controllers.Flights;
+
+public class FlightSearchVm
+{
+    public string? From { get; set; }
+
+    public string? To { get; set; }
+
+    public List<DynamicResource> Flights { get; set; } = new List<DynamicResource>();
+}
--- src/Sample.Gateway/Controllers/Routes.cs
+++ src/Sample.Gateway/Controllers/Routes.cs
@@ -10,4 +10,10 @@ public static class Routes
 
     public static Task<T> GetFlightShoppingSummary<T>(this GatewayRouter router) where T : class
         => router.GetAsync<T>("FlightShopping", "/api/flight-shopping/flights/summary");
+
+    public static Task<T> GetFlightManagementFlights<T>(this GatewayRouter router, string? from, string? to) where T : class
+        => router.GetAsync<T>("FlightManagement", $"/api/flight-management/flights?from={from ?? ""}&to={to ?? ""}");
+
+    public static Task<T> GetFlightShoppingFlight<T>(this GatewayRouter router, int id) where T : class
+        => router.GetAsync<T>("FlightShopping", $"/api/flight-shopping/flights/{id}");
 }
